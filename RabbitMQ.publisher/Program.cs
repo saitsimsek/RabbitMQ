f746f075c9cfcd5633089e56e350f1f0e28a2c99
sait_ <sait_@DESKTOP-UBEO44F>
2023-01-20 19:09:12 +0300

    Add project files.

---
+++ RabbitMQ.publisher/Program.cs
@@ -0,0 +1,22 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://hlhvpexd:8tyFLTYa1wv1adchaD25UOMlJkS7njzJ@shrimp.rmq.cloudamqp.com/hlhvpexd");
+using var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+channel.QueueDeclare("hello-queue", true, false, false);
+
+string message = "Hello world";
+
+for (int i = 0; i < 100; i++)
+{
+    var messageBody = Encoding.UTF8.GetBytes(message+i);
+    channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+    Thread.Sleep(1000); 
+}
+
+//Console.WriteLine("Mesajınız gönderilmiştir.");
+Console.ReadLine();
+
---
+++ RabbitMQ.subscriber/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://hlhvpexd:8tyFLTYa1wv1adchaD25UOMlJkS7njzJ@shrimp.rmq.cloudamqp.com/hlhvpexd");
+using  var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+//channel.QueueDeclare("hello-queue",true,false,false);
+channel.BasicQos(0,1,true);
+var consumer = new EventingBasicConsumer(channel);
+
+channel.BasicConsume("hello-queue",false,consumer);
+
+consumer.Received += (object? sender, BasicDeliverEventArgs e)=>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+    Console.WriteLine("Gelen Mesaj: "+message);
+    channel.BasicAck(e.DeliveryTag,false);
+};
+
+
+Console.ReadLine();
+
